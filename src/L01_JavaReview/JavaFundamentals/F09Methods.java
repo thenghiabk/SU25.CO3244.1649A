97db42ef64c58efedf30c3292bb56541bfb869c7
thenghiabk <thenghiabk@gmail.com>
2025-05-19 23:25:52 +0700

    updated L0_JavaReview

--- src/L01_JavaReview/JavaFundamentals/F09Methods.java
+++ src/L01_JavaReview/JavaFundamentals/F09Methods.java
@@ -7,37 +7,35 @@ public class F09Methods {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Program starts.");
-        greets(scanner);
+
+        String userName = getUserName(scanner);
+        System.out.println("Hello " + userName + "!");
+
         int userAge = getUserAge(scanner);
-        validateAge( userAge );
+
+        boolean isAccessGranted = validateAge(userAge);
+        System.out.println(isAccessGranted ? "Access granted - You are old enough!" : "Access denied - You are not old enough!");
+
         System.out.println("Program ends.");
 
         scanner.close();
     }
 
-    public static void greets(Scanner scanner){
-        System.out.println("Enter your name: ");
+    public static String getUserName(Scanner scanner){
+        System.out.print("Enter your name: ");
         String name = scanner.nextLine();
-        System.out.println("Hello " + name + ".");
+        return name;
     }
 
     public static int getUserAge(Scanner scanner){
-        System.out.println("How old are you?");
+        System.out.print("How old are you? ");
         String ageString = scanner.nextLine();
-        System.out.println("Your age is " + ageString);
         int age = Integer.parseInt(ageString);
         return age;
     }
 
-    public static void validateAge(int age){
-        // If age is less than 18, print "access denied"
-        if (age < 18) {
-            System.out.println("Access denied - You are not old enough!");
-
-            // If age is greater than, or equal to, 18, print "access granted"
-        } else {
-            System.out.println("Access granted - You are old enough!");
-        }
+    public static boolean validateAge(int age) {
+        return age >= 18;
     }
 
 
