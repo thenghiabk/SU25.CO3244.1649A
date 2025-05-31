9e4062cc0391d549446f6c37781429b59a7f7646
thenghiabk <thenghiabk@gmail.com>
2025-05-31 10:58:41 +0700

    update OOPPrinciples

--- src/L01_JavaReview/OOPPrinciples/Light.java
+++ src/L01_JavaReview/OOPPrinciples/Light.java
@@ -6,6 +6,73 @@ public class Light {
     private boolean isOn;
     private int brightness;
 
+    // constructor
+    public Light() {
+        this.name = "Light";
+        this.isOn = false;
+        this.brightness = 50;
+    }
+
+    public Light(String name, int brightness, boolean isOn) {
+        setName( name );
+        setBrightness( brightness );
+        if (isOn) {
+            turnOn();
+        } else {
+            turnOff();
+        }
+    }
+
+    // methods
+    public void turnOn() {
+        isOn = true;
+    }
+
+
+    public void turnOff() {
+        isOn = false;
+    }
+
+    public void setName(String name){
+        this.name = name.toUpperCase();
+    }
+
+    public void setBrightness(int brightness){
+        if (brightness > 0 && brightness <= 100) {
+            this.brightness = brightness;
+        }
+    }
+
+    public void displayInfo() {
+        String status = this.isOn ? "on" : "off";
+        System.out.println(this.name + " is " + status + " with brightness " + this.brightness + "%");
+    }
+}
+
+class AutoLight{
+    // attributes
+    private String name;
+    private boolean isOn;
+    private int brightness;
+    private int timer;
+
+    // constructor
+    public AutoLight() {
+        this.name = "Light";
+        this.isOn = false;
+        this.brightness = 50;
+    }
+
+    public AutoLight(String name, int brightness, boolean isOn) {
+        setName( name );
+        setBrightness( brightness );
+        if (isOn) {
+            turnOn();
+        } else {
+            turnOff();
+        }
+    }
+
     // methods
     public void turnOn() {
         isOn = true;
@@ -16,13 +83,17 @@ public class Light {
     }
 
     public void setName(String name){
-        this.name = name;
+        this.name = name.toUpperCase();
     }
 
     public void setBrightness(int brightness){
         if (brightness > 0 && brightness <= 100) {
             this.brightness = brightness;
         }
+    }
+
+    public void setTimer(int timer){
+        this.timer = timer;
     }
 
     public void displayInfo() {
@@ -39,10 +110,13 @@ class LightTestDrive {
         light1.turnOn();
         light1.displayInfo(); // prints "Bedroom is on with brightness 50%"
 
-        Light light2 = new Light();
-        light2.setName("Bathroom");
-        light2.setBrightness(75);
-        light2.turnOff();
+        Light light2 = new Light("Bathroom", 75, false);
         light2.displayInfo(); // prints "Bathroom is off with brightness 75%"
+
+        AutoLight autoLight1 = new AutoLight();
+        autoLight1.setName("Livingroom");
+        autoLight1.setBrightness(50);
+        autoLight1.setTimer(30);
+        autoLight1.turnOn();
     }
 }
