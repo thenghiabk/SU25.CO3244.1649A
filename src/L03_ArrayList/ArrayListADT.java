2a549ce4f6b7ed2a8acf665602e9810476875444
thenghiabk <thenghiabk@gmail.com>
2025-06-02 17:34:31 +0700

    update ArrayListADT

--- src/L03_ArrayList/ArrayListADT.java
+++ src/L03_ArrayList/ArrayListADT.java
@@ -1,9 +1,31 @@
 package L03_ArrayList;
 
 public class ArrayListADT<E> implements AbstractList<E> {
+    // attributes
+    private E[] elements;
+    private int nextIndex;
+
+    // constructor
+    public ArrayListADT () {
+        this.elements = (E[]) new Object[3];
+        this.nextIndex = 0;
+    }
+
 
     @Override
     public boolean add ( E element ) {
+        if (nextIndex == elements.length) {
+            E[] biggerElements = (E[]) new Object[elements.length * 2];
+
+            for ( int i = 0; i < elements.length; i++ ) {
+                biggerElements[i] = elements[i];
+            }
+
+            elements = biggerElements;
+        }
+
+        elements[nextIndex] = element;
+        nextIndex++;
         return false;
     }
 
---
+++ src/L03_ArrayList/ArrayListADTTestDrive.java
@@ -0,0 +1,15 @@
+package L03_ArrayList;
+
+public class ArrayListADTTestDrive {
+    public static void main ( String[] args ) {
+        ArrayListADT<Integer> myIntList = new ArrayListADT<>(); // <Integer>
+        myIntList.add(10);
+        myIntList.add(20);
+        myIntList.add(30);
+        myIntList.add(40);
+
+        ArrayListADT<String> myStringList = new ArrayListADT<>(); // <String>
+        myStringList.add("Hello");
+        myStringList.add("World");
+    }
+}
