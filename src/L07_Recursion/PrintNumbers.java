989baa059dff732f444370c31da74e26a97ef771
thenghiabk <thenghiabk@gmail.com>
2025-07-14 17:00:51 +0700

    update Recursion

--- src/L07_Recursion/PrintNumbers.java
+++ src/L07_Recursion/PrintNumbers.java
@@ -2,7 +2,31 @@ package L07_Recursion;
 
 public class PrintNumbers {
     public static void main ( String[] args ) {
-        printNumber(); // 1, 2, 3, 4, 5
+        printNumber1(); // 1, 2, 3, 4, 5
+    }
+
+    public static void printNumber1(){
+        System.out.println("1");
+        printNumber2();
+    }
+
+    public static void printNumber2(){
+        System.out.println("2");
+        printNumber3();
+    }
+
+    public static void printNumber3(){
+        System.out.println("3");
+        printNumber4();
+    }
+
+    public static void printNumber4(){
+        System.out.println("4");
+        printNumber5();
+    }
+
+    public static void printNumber5(){
+        System.out.println("5");
     }
 
 }
